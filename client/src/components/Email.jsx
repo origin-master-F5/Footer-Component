757d3733e2b67a8d86a2925047ee899522e29953
Darth Varg <cbvargas123@gmail.com>
2020-03-28 17:53:36 -0700

    Email Component is complete; might go back and add SignUp button functionality

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -10,8 +10,40 @@ class Email extends React.Component {
 
     render() {
         return (
-            <div>
-
+            <div className="email-parent-div">
+                <div className="email-center-wrapper">
+                    <div className="form-wrapper">
+                        <form className="email-form">
+                            <div className="email-title">
+                                Get the latest deals and more.
+                            </div>
+                            <div className="input-wrapper">
+                                <span className="input-overlay-wrapper">
+                                    <span className="input-overlayTrigger">
+                                        <div className="input-parent">
+                                            <input type="text" className="email-input" placeholder="Enter email address" defaultValue="" />
+                                            <button type="submit" className="submit-email-btn" >
+                                                Sign Up
+                                            </button>
+                                        </div>
+                                    </span>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="email-divider-parent">
+                        <div className="email-divider"></div>
+                    </div>
+                    <div className="bby-logo-div">
+                        <div className="bby-logo-wrapper">
+                            <img className="bby-logo" alt="bby-logo" src="./images/app-logo.png" />
+                            <a className="app-entry" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                <span class="app-entry-label">Best Buy app</span>
+                                <span class="app-entry-action">Learn more ›</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
